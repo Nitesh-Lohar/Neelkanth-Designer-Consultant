bdde01ccc9a0ae1e3cf7c78b53b89be967224ddc
Nitesh Lohar <lohar.nitesh.nl99@gmail.com>
2026-05-05 21:15:42 +0530

    Fix Netlify deployment blank page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/Neelkanth-Designer-Consultant"> {/* or "/" for custom domain */}
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
